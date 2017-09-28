d88c4dd04d1ca1720536e3d2fd8fe8666ad705c8
Adam Dyson <contact@adamdyson.com.au>
2017-09-28 22:05:25 +1000

    Simplified testing role using Travis CI and docker containers.

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -2,3 +2,6 @@
 
 # Verify the installed version of Git.
 docker exec --tty ${container_id} env TERM=xterm git --version
+
+# Verify the user account Git configuration.
+docker exec --tty ${container_id} env TERM=xterm cat /home/adam/.gitconfig
