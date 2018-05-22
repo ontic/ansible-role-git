51951a1a405ea1c1c4d9f7ae38f4f154a0968eaa
Adam Dyson <adam.dyson@outlook.com>
2018-05-22 23:10:13 +1000

    Added Ansible 2.4 support

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
+# See the COPYING file bundled with this package for license details.
 
-# Verify the installed version of Git.
+# Verify the installed location.
+docker exec --tty ${container_id} env TERM=xterm which git
+# Verify the installed version.
 docker exec --tty ${container_id} env TERM=xterm git --version
-
 # Verify the user account Git configuration.
 docker exec --tty ${container_id} env TERM=xterm cat /home/adam/.gitconfig
